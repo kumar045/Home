060b91e198ca6274c957837beac25715f5545ff8
Shivam Kumar <35759554+kumar045@users.noreply.github.com>
2023-06-08 02:15:38 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,3 +1,4 @@
 import streamlit as st
-st.write("Due to overload on the server I am shifting my app here")
+st.write("Due to overload on the server I shifted my app here")
+st.write("Please check")
 st.write("https://aibasedtools.azurewebsites.net")
